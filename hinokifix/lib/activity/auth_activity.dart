dc000439b8d48650f0f6db6863901af0f921a0c7
jam <djamilsharipov2010@gmail.com>
2025-11-20 18:13:59 +0300

    1.1.0.2 Полный дизайн auth

--- hinokifix/lib/activity/auth_activity.dart
+++ hinokifix/lib/activity/auth_activity.dart
@@ -13,20 +13,140 @@ class _AuthState extends State<Auth> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
 
-  void _login() {
-    
+  void _login() async {
+    try {
+      UserCredential user = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(
+            email: _emailController.text.trim(),
+            password: _passwordController.text.trim(),
+          );
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Вы успешно вошли в Hinoki.'),
+          duration: Duration(seconds: 4),
+        ),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Ошибка авторизации: $e"),
+          duration: Duration(seconds: 4),
+        ),
+      );
+    }
   }
-  
-  void _register() {
-    
+
+  void _register() async {
+    try {
+      UserCredential user = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+            email: _emailController.text.trim(),
+            password: _passwordController.text.trim(),
+          );
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Вы успешно зарегестрировались."),
+          duration: Duration(seconds: 4),
+        ),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Ошибка регистрации: $e'),
+          duration: Duration(seconds: 4),
+        ),
+      );
+    }
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Color.fromARGB(255, 14, 13, 12),
-        appBar: AppBar(backgroundColor: Color.fromARGB(255, 14, 13, 12), title: Padding(padding: EdgeInsetsGeometry.only(top: 20, left: 15), child: Row(children: [Text(">> Hinoki.auth", style: GoogleFonts.tiny5(color: Color.fromARGB(255, 190, 56, 79), fontSize: 34, fontWeight: FontWeight.w800),), ],)),),
-        body: Column(mainAxisAlignment: MainAxisAlignment.center ,children: [Text("Привет! Авторизуйся в Hinoki", style: GoogleFonts.tiny5(fontSize: 25, color: Color.fromARGB(255, 190, 56, 79)),),  Container(margin: EdgeInsets.only(left: 60, right: 60, top: 10, bottom: 10), child: TextField(controller: _emailController, decoration: InputDecoration(labelText: "e-mail", labelStyle: GoogleFonts.tiny5(), border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(1000)))),),), Container(margin: EdgeInsets.only(left: 60, right: 60, top: 5, bottom: 10), child: TextField(controller: _passwordController, decoration: InputDecoration(labelText: "пароль", labelStyle: GoogleFonts.tiny5(), border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(1000)))),),), Padding(padding: EdgeInsetsGeometry.fromLTRB(10, 10, 10, 10), child: ElevatedButton(onPressed: _login, child: Text("Авторизация", style: GoogleFonts.tiny5(),)),),  Padding(padding: EdgeInsetsGeometry.fromLTRB(10, 10, 10, 10), child: ElevatedButton(onPressed: _register, child: Text("Регистрация", style: GoogleFonts.tiny5(),), style: ElevatedButton.styleFrom(),),),],),
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 14, 13, 12),
+          title: Padding(
+            padding: EdgeInsetsGeometry.only(top: 20, left: 15),
+            child: Row(
+              children: [
+                Text(
+                  ">> Hinoki.auth",
+                  style: GoogleFonts.tiny5(
+                    color: Color.fromARGB(255, 190, 56, 79),
+                    fontSize: 34,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FittedBox(
+              fit: BoxFit.scaleDown,
+              child: Text(
+                "Привет! Авторизуйся в Hinoki",
+                style: GoogleFonts.tiny5(
+                  fontSize: 20,
+                  color: Color.fromARGB(255, 190, 56, 79),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 60, right: 60, top: 10, bottom: 10),
+              child: TextField(
+                style: GoogleFonts.tiny5(color: Colors.white),
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: "e-mail",
+                  labelStyle: GoogleFonts.tiny5(),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(1000)),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 60, right: 60, top: 5, bottom: 10),
+              child: TextField(
+                style: GoogleFonts.tiny5(color: Colors.white),
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: "пароль",
+                  labelStyle: GoogleFonts.tiny5(),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(1000)),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsetsGeometry.fromLTRB(10, 10, 10, 10),
+              child: ElevatedButton(
+                onPressed: _login,
+                child: Text("Авторизация", style: GoogleFonts.tiny5()),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 250, 213, 255),
+                  foregroundColor: Color.fromARGB(255, 202, 0, 108),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsetsGeometry.fromLTRB(10, 10, 10, 10),
+              child: ElevatedButton(
+                onPressed: _register,
+                child: Text("Регистрация", style: GoogleFonts.tiny5()),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 250, 213, 255),
+                  foregroundColor: Color.fromARGB(255, 202, 0, 108),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
