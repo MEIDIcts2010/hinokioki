e8ef7dd54e0ce1e975986193ad32e61fd7664886
jam <djamilsharipov2010@gmail.com>
2025-11-22 00:00:09 +0300

    1.2.0.2 Полный дизайн today, добавлено слово дня

--- hinokifix/lib/activity/today_activity.dart
+++ hinokifix/lib/activity/today_activity.dart
@@ -1,5 +1,5 @@
 import 'dart:math';
-
+import 'package:hinokifix/assets/meme_dart_full.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
@@ -16,6 +16,10 @@ class _TodayState extends State<Today> {
     final now = DateTime.now();
     return DateFormat("d MMMM").format(now);
   }
+  
+  void _addNote() {
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +30,7 @@ class _TodayState extends State<Today> {
         title: Padding(
           padding: EdgeInsetsGeometry.only(top: 20, left: 15, bottom: 20),
           child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(
                 ">> Hinoki.today",
@@ -35,14 +40,56 @@ class _TodayState extends State<Today> {
                   fontWeight: FontWeight.w800,
                 ),
               ),
+              IconButton(
+                onPressed: () {
+                  _addNote();
+                },
+                icon: Icon(Icons.add_circle_outline),
+                color: Color.fromARGB(255, 190, 56, 79),
+                iconSize: 30,
+              ),
             ],
           ),
         ),
-        bottom: PreferredSize(preferredSize: Size.fromHeight(1), child: Container(color: Color.fromARGB(125, 22, 22, 22 ), height: 3)),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(1),
+          child: Container(color: Color.fromARGB(125, 22, 22, 22), height: 3),
+        ),
       ),
-      body: Column(children: [
-        Row(mainAxisAlignment: MainAxisAlignment.center, children: [Column(children: [Text(getFormattedDate(), style: GoogleFonts.preahvihear(color: Color.fromARGB(255, 190, 56, 79), fontSize: 48),), Text(getFormattedDate(), style: GoogleFonts.preahvihear(color: Color.fromARGB(255, 190, 56, 79), fontSize: 48),)],)],)
-      ],)
+      body: Column(
+        children: [
+          Expanded(
+            child: 
+                SingleChildScrollView(child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start, // важно!
+                  children: [
+                    Text(
+                      getFormattedDate(),
+                      style: TextStyle(
+                        fontFamily: 'NuromMedium',
+                        color: Color.fromARGB(255, 189, 43, 67),
+                        fontSize: 48,
+                      ),
+                    ),
+                    Text(
+                      memeText(),
+                      style: GoogleFonts.jost(
+                        color: const Color.fromARGB(255, 173, 31, 55),
+                        fontSize: 32,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Align(alignment: Alignment.centerLeft, child: Padding(padding: EdgeInsetsGeometry.only(left: 40), child: Text("Заметки:", style: GoogleFonts.pixelifySans(color: Color.fromARGB(255, 190, 56, 79), fontSize: 24),),),),
+                    Padding(padding: EdgeInsetsGeometry.only(top: 2, left: 25, right: 25), child: Card(color: Color.fromARGB(255, 33, 30, 36), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)), child: Container(width: double.infinity, height: 150),),),
+                    Align(alignment: Alignment.centerLeft, child: Padding(padding: EdgeInsetsGeometry.only(left: 40), child: Text("Проекты:", style: GoogleFonts.pixelifySans(color: Color.fromARGB(255, 190, 56, 79), fontSize: 24),),),),
+                    Padding(padding: EdgeInsetsGeometry.only(top: 2, left: 25, right: 25), child: Card(color: Color.fromARGB(255, 33, 30, 36), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)), child: Container(width: double.infinity, height: 150),),)
+                  ],
+                ),)
+                
+          ),
+        ],
+      )
     );
   }
 }
